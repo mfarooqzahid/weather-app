f2f0a82eddfc77e281e0cf592337e87b3bff051e
Farooq Zahid <farooqinfocus@gmail.com>
2025-06-14 08:41:32 +0500

    fix: app was not loading

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'application/presentation/home_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
